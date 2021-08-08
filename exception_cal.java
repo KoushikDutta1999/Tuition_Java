e1008e50d7ee520b3d6219e8fa030917eee6e534
Koushik Dutta <koushikdutta.bca2019@nsec.ac.in>
2021-08-08 21:00:29 +0530

    first commit

--- exception_cal.java
+++ exception_cal.java
@@ -11,47 +11,65 @@ public class exception_cal {
         char cal = sc.next().charAt(0);
         float c;
         sc.close();
-        switch (cal) {
-            case '+':
-                c = a + b;
-                try {
-                    if (a < 0 && b < 0)
-                        throw new ArithmeticException("a and b should be positive number");
-                } catch (Exception p) {
-                    System.out.println(p);
+        try {
+            if (a < 0 || b < 0) {
+                throw new Exception("value must be positive");
+            } else {
+                switch (cal) {
+                    case '+':
+                        try {
+                            if (a > 0 || b > 0) {
+                                c = a + b;
+                                System.out.println("Addition " + c);
+                            } else {
+                                throw new ArithmeticException("1st and 2nd number should be positive number");
+                            }
+                        } catch (Exception p) {
+                            System.out.println(p);
+                        }
+                        break;
+                    case '-':
+                        try {
+                            if (a < b) {
+                                throw new ArithmeticException("2nd number should be greater than 1st number");
+                            } else {
+                                c = a - b;
+                                System.out.println("Substraction " + c);
+                            }
+                        } catch (Exception q) {
+                            System.out.println(q);
+                        }
+                        break;
+                    case '*':
+                        try {
+                            if (a == 0 || b == 0) {
+                                throw new ArithmeticException("Both number should be positive");
+                            } else {
+                                c = a * b;
+                                System.out.println("Multiplication " + c);
+                            }
+                        } catch (Exception r) {
+                            System.out.println(r);
+                        }
+                        break;
+                    case '/':
+                        try {
+                            if (b == 0) {
+                                throw new ArithmeticException("2nd number cannot be 0");
+                            } else {
+                                c = a / b;
+                                System.out.println("division " + c);
+                            }
+                        } catch (Exception s) {
+                            System.out.println(s);
+                        }
+                        break;
+                    default:
+                        System.out.println("Wrong input");
                 }
-                System.out.println("Addition " + c);
-                break;
-            case '-':
-                c = a - b;
-                try {
-                    if (b > a)
-                        throw new ArithmeticException("b should be greater than a");
-                } catch (Exception q) {
-                    System.out.println(q);
-                }
-                System.out.println("Substraction " + c);
-                break;
-            case '*':
-                c = a * b;
-                try {
-                    if (a == 0 && b == 0)
-                        throw new ArithmeticException("A and B should be positive");
-                } catch (Exception r) {
-                    System.out.println(r);
-                }
-                System.out.println("Multiplication " + c);
-                break;
-            case '/':
-                c = a / b;
-                try {
-                    if (b == 0)
-                        throw new ArithmeticException("b cannot be 0");
-                } catch (Exception s) {
-                    System.out.println(s);
-                }
-                System.out.println("division " + c);
-                break;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
